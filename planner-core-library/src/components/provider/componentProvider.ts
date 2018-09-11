589257e7a2d2de2e4031c0217f82fb7587edb6c1
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2018-09-11 13:46:34 +0200

    Rewrite ComponentProvider, add Tabs and Headline

--- planner-core-library/src/components/provider/componentProvider.ts
+++ planner-core-library/src/components/provider/componentProvider.ts
@@ -1,13 +1,11 @@
 import React from 'react';
-import BaseButton from '../base/Button';
 import BaseExpansionPanel from '../base/ExpansionPanel';
 import BaseSelect from '../base/Select';
+import BaseTabs from '../base/RadioButtons';
+import BaseHeadline from '../base/Headline';
+
 type ComponentType<Props> = React.SFC<Props> | React.ComponentClass<Props>;
 
-type ButtonPropsType = {
-  onClick: () => void;
-  text: string;
-};
 type ExpansionPanelPropsType = {
   className?: string;
   children: string | JSX.Element | JSX.Element[];
@@ -21,22 +19,64 @@ type SelectPropsType = {
   }[];
   onChange: (event: React.FormEvent<HTMLSelectElement>) => void;
 };
+type TabsPropsType = {
+  selected?: string;
+  items: {
+    id: string;
+    text: string;
+    color: string;
+  }[];
+  onChange: (id: string) => void;
+};
+type HeadlinePropsType = {
+  caption: string;
+};
 
 type ComponentMapType = {
   expansionPanel: ComponentType<ExpansionPanelPropsType>;
   select: ComponentType<SelectPropsType>;
-};
-let componentMap: ComponentMapType = {
-  expansionPanel: BaseExpansionPanel,
-  select: BaseSelect
+  tabs: ComponentType<TabsPropsType>;
+  headline: ComponentType<HeadlinePropsType>;
 };
 
-export function setupComponentMap(customComponents: Partial<ComponentMapType>) {
-  componentMap = {
-    ...componentMap,
-    ...customComponents
+class ComponentProvider {
+  defaultComponentMap: ComponentMapType = {
+    expansionPanel: BaseExpansionPanel,
+    select: BaseSelect,
+    tabs: BaseTabs,
+    headline: BaseHeadline
   };
+  customComponentMap: Partial<ComponentMapType> = {};
+
+  setupComponentMap(customComponents: Partial<ComponentMapType>) {
+    this.customComponentMap = customComponents;
+  }
+
+  getElement(id: keyof ComponentMapType) {
+    return this.customComponentMap[id] || this.defaultComponentMap[id];
+  }
+
+  get ExpansionPanel() {
+    return this.getElement('expansionPanel');
+  }
+
+  get Select() {
+    return this.getElement('select');
+  }
+
+  get Tabs() {
+    return this.getElement('tabs');
+  }
+
+  get Headline() {
+    return this.getElement('headline');
+  }
 }
 
-export const ExpansionPanel = componentMap.expansionPanel;
-export const Select = componentMap.select;
+const componentProvider = new ComponentProvider();
+
+export default componentProvider;
+
+export function setupComponentMap(customComponents: Partial<ComponentMapType>) {
+  componentProvider.setupComponentMap(customComponents);
+}
