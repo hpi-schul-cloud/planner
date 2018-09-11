473c8336d65674645e591bd2b98bc1f87d9cdde5
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2018-09-11 20:09:34 +0200

    Include material Button in exports

--- planner-core-library/stories/materialComponents/index.ts
+++ planner-core-library/stories/materialComponents/index.ts
@@ -2,5 +2,6 @@ import ExpansionPanel from './ExpansionPanel';
 import Select from './Select';
 import Tabs from './Tabs';
 import Headline from './Headline';
+import Button from './Button';
 
-export { ExpansionPanel, Select, Tabs, Headline };
+export { ExpansionPanel, Select, Tabs, Headline, Button };
