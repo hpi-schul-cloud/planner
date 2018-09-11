43278ad0e5d9609333c200492ec9c0d76cca16c3
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2018-09-11 13:43:53 +0200

    Fix passing of color to svg icons

--- planner-core-library/src/assets/IconArrowUp.tsx
+++ planner-core-library/src/assets/IconArrowUp.tsx
@@ -9,7 +9,7 @@ const SvgComponent = (props: PropsType) => (
     <title>path5990</title>
     <path
       d="M1.02 7.37a.99.99 0 0 1-.935-.6 1.019 1.019 0 0 1 .192-1.105L5.225.317a.984.984 0 0 1 1.444 0l4.948 5.348c.255.26.35.64.25.992a.998.998 0 0 1-.735.7.982.982 0 0 1-.96-.314L5.947 2.477 1.722 7.043a.985.985 0 0 1-.701.327z"
-      fill="color"
+      fill={props.color}
       fillRule="evenodd"
     />
   </svg>
---
+++ planner-core-library/src/assets/IconTrash.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type PropsType = {
+  color: string;
+};
+
+const IconTrash: React.SFC<PropsType> = props => (
+  <svg width={24} height={24} {...props}>
+    <path
+      d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6v12z"
+      fill={props.color}
+    />
+  </svg>
+);
+
+export default IconTrash;
