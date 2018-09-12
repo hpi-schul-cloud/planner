7b2a81d2d4fd6b49e09da9b69d7d0ca290d099b0
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2018-09-12 13:31:35 +0200

    Add a general styles provider

---
+++ planner-core-library/src/components/provider/generalStylesProvider.ts
@@ -0,0 +1,39 @@
+type GeneralStylesType = {
+  'font-family': string;
+  primaryColor: string;
+  secondaryColor: string;
+  defaultTextColor: string;
+  strongTextColor: string;
+  lightTextColor: string;
+};
+
+class GeneralStylesProvider {
+  defaultStyles: GeneralStylesType = {
+    'font-family': 'sans-serif',
+    primaryColor: '#b10438',
+    secondaryColor: '#009688',
+    defaultTextColor: '#5e5e5e',
+    strongTextColor: '#4a4a4a',
+    lightTextColor: '#9a9a9a'
+  };
+  customStyles: {} = {};
+
+  setupCustomStyles(customStyles: Partial<GeneralStylesType>) {
+    this.customStyles = customStyles;
+  }
+
+  get generalStyles() {
+    return {
+      ...this.defaultStyles,
+      ...this.customStyles
+    };
+  }
+}
+
+const stylesProvider = new GeneralStylesProvider();
+
+export default stylesProvider;
+
+export function setupCustomStyles(customStyles: Partial<GeneralStylesType>) {
+  stylesProvider.setupCustomStyles(customStyles);
+}
