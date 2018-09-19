954a1b6979849d790fd5b8cd739cd3aadd249c93
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2018-09-19 17:21:51 +0200

    Add types file

---
+++ planner-core-library/src/components/types.ts
@@ -0,0 +1,37 @@
+export type AllClassInstancesType = {
+  [subjecId: string]: {
+    subjectId: string;
+    subjectName: string;
+    classLevels: {
+      [classLevelId: string]: {
+        classLevelId: string;
+        classLevelName: string;
+        classes: ClassInstanceType;
+      };
+    };
+  };
+};
+
+export type ClassInstanceType = {
+  [classId: string]: {
+    id: string;
+    name: string;
+    topics: TopicElementsType[];
+  };
+};
+
+export type TopicElementsType = {
+  id: string;
+  text: string;
+  color: string;
+  startDate: number;
+  endDate: number;
+};
+
+// Data type for events
+export type EventType = {
+  name: string;
+  color?: string;
+  startDate: number;
+  endDate: number;
+}[];
