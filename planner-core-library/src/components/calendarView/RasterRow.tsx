626ef04b1fd2327be398be5b28f9420a59ac051b
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2018-09-22 12:55:43 +0200

    Add a non interactive RasterRow component

---
+++ planner-core-library/src/components/calendarView/RasterRow.tsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import RasterTopicElement from '../planner/RasterTopicElement';
+
+export type TopicElementsType = {
+  id: string;
+  text: string;
+  color: string;
+  startIndex: number;
+  endIndex: number;
+};
+
+type PropsType = {
+  topicElements: TopicElementsType[];
+  rasterSize: number;
+  rasterCount: number;
+};
+
+const FillerElement = styled.div`
+  width: ${({ width }: { width: number }) => `${width}px`};
+  height: 1px;
+  display: inline-block;
+`;
+
+const RasterRowContainer = styled.div`
+  display: inline-block;
+`;
+
+class InteractiveRasterRow extends Component<PropsType> {
+  generateElements = () => {
+    const { topicElements, rasterSize, rasterCount } = this.props;
+    let elements = [];
+    let nextIndex = 0;
+    let i = 0;
+    for (i = 0; i < topicElements.length; i++) {
+      const { id, text, color, startIndex, endIndex } = topicElements[i];
+
+      if (topicElements[i].startIndex > nextIndex) {
+        elements.push(
+          <FillerElement
+            width={(startIndex - nextIndex) * rasterSize}
+            key={`Filler-${i}`}
+          />
+        );
+      }
+      elements.push(
+        <RasterTopicElement
+          id={id}
+          rasterSize={rasterSize}
+          startIndex={startIndex}
+          endIndex={endIndex}
+          color={color}
+          text={text}
+          key={id}
+          onClick={() => {}}
+          // size
+        />
+      );
+      nextIndex = endIndex + 1;
+    }
+    if (nextIndex < rasterCount) {
+      elements.push(
+        <FillerElement
+          width={(rasterCount - nextIndex) * rasterSize}
+          key={`Filler-${i}`}
+        />
+      );
+    }
+
+    return elements;
+  };
+
+  render() {
+    const elements = this.generateElements();
+
+    return <RasterRowContainer>{elements}</RasterRowContainer>;
+  }
+}
+
+export default InteractiveRasterRow;
