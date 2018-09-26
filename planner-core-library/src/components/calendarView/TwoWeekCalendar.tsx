1e7dc31a11e38e5e05d0ab6cb30a0c2bc4ce6b34
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2018-09-26 18:49:36 +0200

    Add TwoWeekCalendar

---
+++ planner-core-library/src/components/calendarView/TwoWeekCalendar.tsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import RowCaptions from './RowCaptions';
+import DayClassRows from './DayClassRows';
+import {
+  ClassTopicsDataType,
+  filterRelevantElementData,
+  getFilteredClassTopicsData,
+  getWeekPeriodForDate
+} from './calendarHelper';
+import { EventType } from '../types';
+
+type PropsType = {
+  rasterSize: number;
+  utcToday: number;
+  classTopicsData: ClassTopicsDataType;
+  holidaysData: EventType;
+  otherEventsData: EventType;
+  onTopicInstanceClick: (id: string) => void;
+};
+
+const StyledFlexContainer = styled.div`
+  display: flex;
+`;
+const StyledRowCaptions = styled(RowCaptions)`
+  padding-top: 31px;
+`;
+
+class TwoWeekCalendar extends Component<PropsType> {
+  render() {
+    const {
+      utcToday,
+      rasterSize,
+      classTopicsData,
+      holidaysData,
+      otherEventsData,
+      onTopicInstanceClick
+    } = this.props;
+    const twoWeekTimePeriod = getWeekPeriodForDate(utcToday, 2);
+    const labels = classTopicsData.map(classTopic => ({
+      topLabel: classTopic.className,
+      subLabels: classTopic.classes.map(classTopic => classTopic.subjectName)
+    }));
+    const filteredHolidaysData = filterRelevantElementData(
+      twoWeekTimePeriod.utcStartDate,
+      twoWeekTimePeriod.utcEndDate,
+      holidaysData
+    );
+    const filteredOtherEventsData = filterRelevantElementData(
+      twoWeekTimePeriod.utcStartDate,
+      twoWeekTimePeriod.utcEndDate,
+      otherEventsData
+    );
+    const filteredClassTopicsData = getFilteredClassTopicsData(
+      classTopicsData,
+      twoWeekTimePeriod
+    );
+
+    return (
+      <div>
+        <StyledFlexContainer>
+          <StyledRowCaptions labels={labels} />
+          <DayClassRows
+            rasterSize={rasterSize}
+            rowPeriod={twoWeekTimePeriod}
+            utcToday={utcToday}
+            classTopicsData={filteredClassTopicsData}
+            holidaysData={filteredHolidaysData}
+            otherEventsData={filteredOtherEventsData}
+            onTopicInstanceClick={onTopicInstanceClick}
+          />
+        </StyledFlexContainer>
+      </div>
+    );
+  }
+
+  static defaultProps = {
+    rasterSize: 50,
+    utcToday: 1537308000000,
+    holidaysData: [],
+    otherEventsData: [],
+    onTopicInstanceClick: () => {}
+  };
+}
+
+export default TwoWeekCalendar;
