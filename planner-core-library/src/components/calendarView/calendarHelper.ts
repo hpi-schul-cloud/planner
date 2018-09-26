79fd83910ec66697f8e09d37cf8aa7e44fba9811
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2018-09-26 18:44:26 +0200

    Extract some functions to calendarHelper

---
+++ planner-core-library/src/components/calendarView/calendarHelper.ts
@@ -0,0 +1,63 @@
+import { TopicElementsType } from '../types';
+const DAY = 1000 * 60 * 60 * 24;
+
+export type ClassTopicsDataType = {
+  className: string;
+  classes: {
+    subjectId: string;
+    subjectName: string;
+    topics: TopicElementsType[];
+  }[];
+}[];
+
+export function filterRelevantElementData<
+  K extends { utcStartDate: number; utcEndDate: number }
+>(utcStartDate: number, utcEndDate: number, elements: K[]) {
+  return elements.filter(
+    element =>
+      (element.utcStartDate < utcStartDate &&
+        element.utcEndDate > utcStartDate) ||
+      (element.utcStartDate >= utcStartDate &&
+        element.utcStartDate < utcEndDate)
+  );
+}
+
+export const getWeekPeriodForDate = (date: number, numberOfWeeks: number) => {
+  const nonUtcDate = new Date(date);
+  const utcDate = new Date(
+    Date.UTC(
+      nonUtcDate.getUTCFullYear(),
+      nonUtcDate.getUTCMonth(),
+      nonUtcDate.getUTCDate()
+    )
+  );
+  const periodStart =
+    utcDate.getUTCDay() === 0
+      ? utcDate.getTime() - 6 * DAY
+      : utcDate.getTime() - (utcDate.getUTCDay() - 1) * DAY;
+  const periodEnd = periodStart + (7 * numberOfWeeks - 1) * DAY;
+  return { utcStartDate: periodStart, utcEndDate: periodEnd };
+};
+
+export const getFilteredClassTopicsData = (
+  classTopics: ClassTopicsDataType,
+  {
+    utcStartDate,
+    utcEndDate
+  }: {
+    utcStartDate: number;
+    utcEndDate: number;
+  }
+) => {
+  return classTopics.map(classTopic => ({
+    ...classTopic,
+    classes: classTopic.classes.map(singleClass => ({
+      ...singleClass,
+      topics: filterRelevantElementData(
+        utcStartDate,
+        utcEndDate,
+        singleClass.topics
+      )
+    }))
+  }));
+};
