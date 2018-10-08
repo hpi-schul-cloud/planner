ab9de1f6adcdf78082ca7bec5ddc312cb4f94f28
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2018-10-08 18:31:38 +0200

    Add edit icon, modify trash icon

--- planner-core-library/src/assets/IconTrash.tsx
+++ planner-core-library/src/assets/IconTrash.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 type PropsType = {
-  color: string;
+  width?: number;
+  height?: number;
+  color?: string;
 };
 
 const IconTrash: React.SFC<PropsType> = props => (
-  <svg width={24} height={24} {...props}>
+  <svg width={24} height={24} viewBox="0 0 24 24" {...props}>
     <path
       d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6v12z"
-      fill={props.color}
+      fill={props.color || 'currentColor'}
     />
   </svg>
 );
