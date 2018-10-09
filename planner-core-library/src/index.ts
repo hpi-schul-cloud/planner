a931ffeb068de26df60ff9d93c2fe941e09acb3d
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2018-10-09 11:32:10 +0200

    Export everything necessary

--- planner-core-library/src/index.ts
+++ planner-core-library/src/index.ts
@@ -3,11 +3,15 @@ import {
   TopicTemplateView,
   TopicInstanceView
 } from './components/topicTemplateView';
+import { setupCustomStyles } from './components/provider/generalStylesProvider';
+import { setupComponentMap } from './components/provider/componentProvider';
 import { CalendarView } from './components/calendarView';
 
 export {
   ClassConfigurationView,
   CalendarView,
   TopicTemplateView,
-  TopicInstanceView
+  TopicInstanceView,
+  setupCustomStyles,
+  setupComponentMap
 };
