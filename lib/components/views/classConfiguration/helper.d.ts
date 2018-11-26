0a25c3eb7346a05b1cec43d699c26a32862e199a
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2018-11-26 23:47:44 +0100

    Rebuild library

--- lib/components/views/classConfiguration/helper.d.ts
+++ lib/components/views/classConfiguration/helper.d.ts
@@ -1,4 +1,6 @@
 import { TopicIndexType } from '../../types';
+import { AllClassInstancesType } from './types';
+export declare const sortAllTopics: (classInstances: AllClassInstancesType<TopicIndexType>) => AllClassInstancesType<TopicIndexType>;
 export declare const getEmptySpaceSize: (rasterCount: number, topicElements: TopicIndexType[]) => number;
 export declare const getClassTopicsAfterInsertion: (insertStartIndex: number, width: number, elementValues: Partial<TopicIndexType>, rasterCount: number, currentClassInstances: TopicIndexType[]) => TopicIndexType[];
 export declare const getClassTopicsAfterMove: (insertStartIndex: number, elementIndex: number, width: number, elementValues: Partial<TopicIndexType>, rasterCount: number, currentClassInstances: TopicIndexType[]) => TopicIndexType[];
